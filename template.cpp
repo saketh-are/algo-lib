82478aa6ce47d25594723c5bde8c157ff3fb0c14
Sakethnath Are <saketh.are@gmail.com>
2015-06-16 20:17:24 -0700

    remove tab chars

--- template.cpp
+++ template.cpp
@@ -27,12 +27,12 @@ using namespace std;
 typedef long long ll;
 
 void setup(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     cout.precision(15);
 }
 
 int main(){
-	setup();
+    setup();
 }
