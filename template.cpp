13a0bb490ba890f9d45c4aa7e86e10654a3b1cfc
Sakethnath Are <saketh.are@gmail.com>
2015-06-16 20:16:12 -0700

    add template

---
+++ template.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+
+void setup(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+    cout.precision(15);
+}
+
+int main(){
+	setup();
+}
