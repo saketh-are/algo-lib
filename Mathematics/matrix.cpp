cbeb5c6484d2b49d38a0a3b0728cf12c239f1829
Sakethnath Are <saketh.are@gmail.com>
2015-06-17 00:53:18 -0700

    refactor matrix

--- Mathematics/matrix.cpp
+++ Mathematics/matrix.cpp
@@ -8,6 +8,7 @@ ll prod(ll a, ll b){ return (a*b) % MOD; }
 struct mat{
     int N;
     ll **dat;
+    
     mat(int _N, ll diag = 0){
         N = _N;
         dat = (ll**) malloc(sizeof(ll*) * N);
@@ -16,30 +17,31 @@ struct mat{
             dat[i][i] = diag;
         }
     }
-    ~mat(){
-        for(int i=0; i<N; i++) free(dat[i]);
-        free(dat);
+
+    void free(){
+        for(int i=0; i<N; i++) ::free(dat[i]);
+        ::free(dat);
     }
 
     ll *&operator [](int i){
         return dat[i];
     }
 
-    mat *operator *(mat &b){
-        mat *r = new mat(N);
+    mat operator *(mat b){
+        mat r(N);
         for(int i=0; i<N; i++)
             for(int j=0; j<N; j++)
                 for(int k=0; k<N; k++)
-                    add((*r)[i][j], prod(dat[i][k], b[k][j]));
+                    add(r[i][j], prod(dat[i][k], b[k][j]));
         return r;
     }
 
-    mat *pow(ll E){
-        if(!E) return new mat(N, 1);
-        mat *b = *this * *this;
-        mat *p = b->pow(E>>1);
-        delete b;
-        if(E&1){ mat *q = *p * *this; delete p; return q; }
+    mat pow(ll E){
+        if(!E) return mat(N, 1);
+        mat b = *this * *this;
+        mat p = b.pow(E>>1);
+        b.free();
+        if(E&1){ mat q = p * *this; p.free(); return q; }
         return p;        
     }
 };
