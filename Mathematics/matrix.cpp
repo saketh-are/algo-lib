bd0474156b5bdd01308d732638ebe4ae6ac226ce
Sakethnath Are <saketh.are@gmail.com>
2015-06-29 04:16:13 -0700

    refactor matrix

--- Mathematics/matrix.cpp
+++ Mathematics/matrix.cpp
@@ -1,5 +1,3 @@
-const ll MOD = 1000000007;
-
 void add(ll &a, ll b){ a = (a+b) % MOD; }
 ll sum(ll a, ll b){ return (a+b) % MOD; }
 void mul(ll &a, ll b){ a = (a*b) % MOD; }
@@ -7,34 +5,18 @@ ll prod(ll a, ll b){ return (a*b) % MOD; }
 
 struct mat{
     int N;
-    ll **dat;
-    
+    vector<vector<ll> > dat;
+
     mat(int _N, ll diag = 0){
         N = _N;
-        dat = (ll**) malloc(sizeof(ll*) * N);
+        dat.resize(N);
         for(int i=0; i<N; i++){
-            dat[i] = (ll*) calloc(N, sizeof(ll));
+            dat[i].resize(N);
             dat[i][i] = diag;
         }
     }
 
-    void free(){
-        for(int i=0; i<N; i++) ::free(dat[i]);
-        ::free(dat);
-    }
-
-    void operator <=(mat o){
-        this->free();
-        this->dat = o.dat;
-    }
-
-    ll ret(int i, int j){
-        ll res = dat[i][j];
-        this->free();
-        return res;
-    }
-
-    ll *&operator [](int i){
+    vector<ll>& operator[](int i){
         return dat[i];
     }
 
@@ -46,14 +28,23 @@ struct mat{
                     add(r[i][j], prod(dat[i][k], b[k][j]));
         return r;
     }
-
+    
     mat pow(ll E){
         if(!E) return mat(N, 1);
-        mat b = *this * *this;
-        mat p = b.pow(E>>1);
-        b.free();
-        if(E&1){ mat q = p * *this; p.free(); return q; }
-        return p;        
+        mat r = (*this * *this).pow(E/2);
+        return E&1 ? r * *this : r;
+    }
+    
+    friend ostream& operator<<(ostream &os, mat &m){
+        os << "{";
+        for(int i=0; i<m.N; i++){
+            if(i) os << "},\n ";
+            os << "{";
+            for(int j=0; j<m.N; j++){
+                if(j) os << ", ";
+                os << setw(10) << m[i][j] << setw(0);
+            }
+        }
+        return os << "}}";
     }
 };
-
