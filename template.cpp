d1d37f7aaa84d60168a62f164dc7de6874ff59fd
Sakethnath Are <saketh.are@gmail.com>
2015-07-02 01:54:59 -0700

    use bits/stdc++.h

--- template.cpp
+++ template.cpp
@@ -1,29 +1,4 @@
-#include <algorithm>
-#include <assert.h>
-#include <bitset>
-#include <chrono>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <deque>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <limits>
-#include <list>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string.h>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
