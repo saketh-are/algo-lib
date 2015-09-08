ab8b99fb40658a23aa2f80d1e3d2205cc47dbba7
Sakethnath Are <saketh.are@gmail.com>
2015-09-08 02:38:20 -0400

    add segtree with range updates

---
+++ Generic/segment_tree_lazy.cpp
@@ -0,0 +1,65 @@
+template<typename T, typename U> struct seg_tree_lazy {
+    int S, H;
+ 
+    T def;
+    vector<T> value;
+   
+    U noop;
+    vector<U> prop;
+ 
+    seg_tree_lazy<T, U>(int _S, T _def, U _noop) {
+        S = _S, def = _def, noop = _noop;
+        H = 32 - __builtin_clz(S);
+ 
+        value.resize(2 * S + 1, def);
+        prop.resize(2 * S + 1, noop);
+    }
+ 
+    void apply(int i, U update) {
+        value[i] = update.apply(value[i]);
+        if(i < S) prop[i] = prop[i] + update;
+    }
+ 
+    void rebuild(int i) {
+        for (int l = i/2; l; l /= 2) {
+            T combined = value[2*l] + value[2*l+1];
+            value[l] = prop[l].apply(combined);
+        }
+    }
+ 
+    void propagate(int i) {
+        for (int h = H; h > 0; h--) {
+            int l = i >> h;
+            if (prop[l] != noop) {
+                apply(2*l, prop[l]);
+                apply(2*l+1, prop[l]);
+                prop[l] = noop;
+            }
+        }
+    }
+ 
+    void upd(int i, int j, U update) {
+        i += S, j += S;
+        propagate(i), propagate(j);
+ 
+        for (int l = i, r = j; l <= r; l /= 2, r /= 2) {
+            if((l&1) == 1) apply(l++, update);
+            if((r&1) == 0) apply(r--, update);
+        }
+ 
+        rebuild(i), rebuild(j);
+    }
+ 
+    T query(int i, int j){
+        i += S, j += S;
+        propagate(i), propagate(j);
+ 
+        T res = def;
+        for(; i <= j; i /= 2, j /= 2){
+            if((i&1) == 1) res = res + value[i++];
+            if((j&1) == 0) res = res + value[j--];
+        }
+        return res;
+    }
+};
+
