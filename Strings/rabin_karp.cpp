25fedb92c2281ddd225c1c7fa3e9d2f8953997b7
Sakethnath Are <saketh.are@gmail.com>
2015-09-18 03:40:42 -0400

    clean up rabin karp a bit

--- Strings/rabin_karp.cpp
+++ Strings/rabin_karp.cpp
@@ -1,27 +1,25 @@
-struct rabin_karp{
-    static const int MAXL = 200200;
-    static const ll SP = 3001, LP = 1e9 + 7;
+namespace rabin_karp{
+    const int MAXL = 200200;
+    const ll SP = 3001, LP = 1e9 + 7;
 
-    static vector<ll> powers;
-    static void init(){
+    vector<ll> powers;
+    void init(){
         powers.resize(MAXL);
         powers[0] = 1;
         for(int i=1; i<MAXL; i++)
             powers[i] = (SP * powers[i-1]) % LP;
     }
 
-    vector<ll> hashes;
-
-    rabin_karp(string str){
-        hashes.resize(str.size()+1);
+    vector<ll> compute(string str){
+        vector<ll> hashes(str.size()+1);
         for(int i=1; i<=str.size(); i++)
             hashes[i] = (SP * hashes[i-1] + int(str[i-1])) % LP;
+        return hashes;
     }
 
-    ll read(int i, int l){
+    ll read(vector<ll> &hashes, int i, int l){
         ll sub = (hashes[i] * powers[l]) % LP;
         return (hashes[i+l] - sub + LP) % LP;
     }
 };
-vector<ll> rabin_karp::powers;
 
