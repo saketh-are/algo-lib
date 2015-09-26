a45443ea6a8e2508069755446c6ac840483eca6c
Sakethnath Are <saketh.are@gmail.com>
2015-09-25 23:02:50 -0400

    segtree update

--- Generic/segment_tree.cpp
+++ Generic/segment_tree.cpp
@@ -1,12 +1,19 @@
 template<typename T> struct seg_tree {
     int S;
 
-    T def;
+    T zero;
     vector<T> value;
 
-    seg_tree<T>(int _S, T _def = T()) {
-        S = _S, def = _def;
-        value.resize(2*S+1, def);
+    seg_tree<T>(int _S, T _zero = T()) {
+        S = _S, zero = _zero;
+        value.resize(2*S+1, zero);
+    }
+
+    void set_leaves(vector<T> &leaves) {
+        copy(leaves.begin(), leaves.end(), value.begin() + S);
+
+        for (int i = S - 1; i > 0; i--) 
+            value[i] = value[2 * i] + value[2 * i + 1];
     }
 
     void upd(int i, T v) {
@@ -19,7 +26,7 @@ template<typename T> struct seg_tree {
     }
 
     T query(int i, int j) {
-        T res_left = def, res_right = def;
+        T res_left = zero, res_right = zero;
         for(i += S, j += S; i <= j; i /= 2, j /= 2){
             if((i&1) == 1) res_left = res_left + value[i++];
             if((j&1) == 0) res_right = value[j--] + res_right;
