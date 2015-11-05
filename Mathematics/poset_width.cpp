b337b3d39d9de70625a0092faada900adf6e85ab
Sakethnath Are <saketh.are@gmail.com>
2015-11-05 17:11:12 -0500

    add poset width

---
+++ Mathematics/poset_width.cpp
@@ -0,0 +1,60 @@
+vector<int> width(vector<vector<int>> poset) {
+    int N = poset.size();
+    bipartite_graph g(N, N);
+
+    for (int i = 0; i < N; i++) {
+        for (int j : poset[i])
+            g.edge(j, i);
+    }
+
+    g.matching();
+
+    vector<bool> vis[2];
+    vis[false].resize(2 * N, false);
+    vis[true].resize(2 * N, false);
+
+    for (int i = 0; i < N; i++) {
+        if (g.match[i] != -1) continue;
+        if (vis[false][i]) continue;
+
+        queue<pair<bool, int>> bfs;
+        bfs.push(make_pair(false, i));
+        vis[false][i] = true;
+
+        while (!bfs.empty()) {
+            bool inm = bfs.front().first;
+            int loc = bfs.front().second;
+            bfs.pop();
+
+            for (int nbr : g.adj[loc]) {
+                if (vis[!inm][nbr]) continue;
+                if ((g.match[loc] == nbr) ^ inm) continue;
+
+                vis[!inm][nbr] = true;
+                bfs.push(make_pair(!inm, nbr));
+            }
+        }
+    }
+
+    vector<bool> inz(2 * N, false);
+    for (int i = 0; i < 2 * N; i++)
+        inz[i] = vis[true][i] || vis[false][i];
+
+    vector<bool> ink(N, false);
+
+    for (int i = 0; i < N; i++)
+        if (!inz[i])
+            ink[i]= true;
+
+    for (int i = N; i < 2 * N; i++) 
+        if (inz[i])
+            ink[i - N] = true;
+
+    vector<int> res;
+    for (int i = 0; i < N; i++) {
+        if (!ink[i])
+            res.push_back(i);
+    }
+    return res;
+}
+
