f689e34ee7da52e78116bdeb5951193b3265c828
Sakethnath Are <saketh.are@gmail.com>
2016-05-02 22:37:01 -0400

    clean up union find a bit

--- Generic/union_find.cpp
+++ Generic/union_find.cpp
@@ -1,21 +1,18 @@
-struct union_find{
+struct union_find {
     vector<int> P, R, S;
     
-    union_find(int N){
-        P.resize(N), R.resize(N), S.resize(N);
-        for(int i=0; i<N; i++){
+    union_find (int N) {
+        P.resize(N), R.resize(N, 0), S.resize(N, 1);
+        for (int i = 0; i < N; i++){
             P[i] = i;
-            R[i] = 0;
-            S[i] = 1;
-        }
     }
     
-    int rep(int i){
+    int rep(int i) {
         if(P[i] != i) P[i] = rep(P[i]);
         return P[i];
     }
 
-    bool unio(int a, int b){
+    bool unio(int a, int b) {
         a = rep(a), b = rep(b);
         if(a == b) return false;
         if(R[a] < R[b]) swap(a, b);
