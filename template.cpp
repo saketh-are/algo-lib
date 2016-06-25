1a8b2cbceb732f7370989a1834e74dc0d0e601a1
Sakethnath Are <saketh.are@gmail.com>
2016-06-25 18:29:29 -0400

    update template

--- template.cpp
+++ template.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define all(x) (x).begin(), (x).end()
+
 #define TRACE(x) x
 #define WATCH(x) TRACE(cout << #x" = " << x << endl)
 #define WATCHR(a, b) TRACE(for (auto c=a; c!=b;) cout << *(c++) << " "; cout << endl)
-#define WATCHC(V) TRACE(cout << #V" = "; WATCHR(V.begin(), V.end())
+#define WATCHC(V) TRACE({cout << #V" = "; WATCHR(V.begin(), V.end());})
 
 typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(0), cout.tie(0), cout.precision(15);
+    cin.tie(0), cout.tie(0);
+    cout << fixed << setprecision(15);
 
     return 0;
 }
