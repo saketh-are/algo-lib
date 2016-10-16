e8dcd7deb3ffe19fc4f538894e02af6679957020
Sakethnath Are <saketh.are@gmail.com>
2016-10-15 17:57:59 -0700

    update template

--- template.cpp
+++ template.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define all(x) (x).begin(), (x).end()
-
 #define TRACE(x) x
 #define WATCH(x) TRACE(cout << #x" = " << x << endl)
 #define WATCHR(a, b) TRACE(for (auto it=a; it!=b;) cout << *(it++) << " "; cout << endl)
 #define WATCHC(V) TRACE({cout << #V" = "; WATCHR(V.begin(), V.end());})
 
+#define all(x) (x).begin(), (x).end()
+
 typedef long long ll;
+typedef vector<bool> vb;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<ll> vll;
@@ -21,4 +22,3 @@ int main() {
 
     return 0;
 }
-
