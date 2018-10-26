78c01e95f8faa6ab0cc01970f23f372e1ecc9ffd
Saketh Are <saketh.are@gmail.com>
2018-10-26 16:35:55 -0400

    binary_indexed_tree: add lower_bound

--- data_structures/binary_indexed_tree.cpp
+++ data_structures/binary_indexed_tree.cpp
@@ -6,21 +6,33 @@ template<typename T> struct BIT {
         S = _S;
         v.resize(S+1);
     }
-    
+
     void update(int i, T k) {
-        for(i++; i<=S; i+=i&-i)
+        for(i++; i <= S; i += i&-i)
             v[i] = v[i] + k;
     }
 
     T read(int i) {
         T sum = 0;
-        for(i++; i; i-=i&-i)
+        for(i++; i; i -= i&-i)
             sum = sum + v[i];
         return sum;
     }
 
     T read(int l, int r) {
         return read(r) - read(l-1);
+    }
+
+    int lower_bound(T sum) {
+        T res = 0;
+        int inx = 0;
+        for (int i = 31 - __builtin_clz(S); i >= 0; i--) {
+            if ((inx + (1 << i) <= S) && res + v[inx + (1 << i)] < sum) {
+                inx += 1 << i;
+                res += v[inx];
+            }
+        }
+        return inx;
     }
 };
 
