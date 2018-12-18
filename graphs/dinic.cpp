e209cb0cac64c9a2a238954f7a61f0fa84c42ec3
Saketh Are <saketh.are@gmail.com>
2018-12-18 15:32:22 -0500

    dinic: return the constructed max flow

--- graphs/dinic.cpp
+++ graphs/dinic.cpp
@@ -1,28 +1,28 @@
 template<typename F> struct dinic {
-    F ZERO, INF;
+    static const F inf = numeric_limits<F>::max();
 
     int V;
     vvi adj;
     vi dest;
     vector<F> cap;
 
-    dinic (int _V = 0, F _zero = 0, F _inf = INT_MAX) :
-            V(_V), ZERO(_zero), INF(_inf) {
+    dinic (int V = 0) : V(V) {
         adj.resize(V);
     }
 
-    void __arc(int u, int v, F _cap) {
+    void __arc(int u, int v, F c) {
         adj[u].push_back(dest.size());
         dest.push_back(v);
-        cap.push_back(_cap);
+        cap.push_back(c);
     }
 
-    void arc(int u, int v, F _cap) {
-        __arc(u, v, _cap);
-        __arc(v, u, ZERO);
+    // Inserts a directed edge u --> v with capacity c.
+    void arc(int u, int v, F c) {
+        __arc(u, v, c);
+        __arc(v, u, F(0));
     }
 
-    bool bfs(int s, int t, vi& level, vector<F>& flow) {
+    bool bfs(int s, int t, vi& level, vector<F>& flow) const {
         level = vi(V, -1);
         level[s] = 0;
         for (queue<int> q({s}); !q.empty(); q.pop()) {
@@ -37,32 +37,38 @@ template<typename F> struct dinic {
         return level[t] != -1;
     }
 
-    F augment(int s, int t, vi& level, vector<F>& flow, vi& inx, F cur) {
+    F augment(int s, int t, vi& level, vector<F>& flow, vi& inx, F cur) const {
         if (s == t) return cur;
         for (int e; inx[s] < adj[s].size(); inx[s]++) {
             e = adj[s][inx[s]];
             if (level[dest[e]] != level[s] + 1) continue;
             if (flow[e] == cap[e]) continue;
             F incr = augment(dest[e], t, level, flow, inx, min(cur, cap[e] - flow[e]));
-            if (incr > ZERO) {
+            if (incr > F(0)) {
                 flow[e] += incr;
                 flow[e^1] -= incr;
                 return incr;
             }
         }
-        return ZERO;
+        return F(0);
     }
 
-    // O(V^2 E)
-    F max_flow(int s, int t) {
+    /*
+     * Computes a maximum flow from node s to node t.
+     *
+     * Runs in O(V^2 * E) in the general case.
+     * Runs in O(min{ V^(2/3), E^(1/2) } * E) if all edges have unit capacity.
+     * Runs in O(V^(1/2) * E) for bipartite matching.
+     */
+    tuple<F, vector<F>> max_flow(int s, int t) const {
         assert(s != t);
-        F res = ZERO;
+        F res(0);
         vector<F> flow(cap.size());
         for (vi level; bfs(s, t, level, flow); ) {
-            for (vi inx(V, 0); F incr = augment(s, t, level, flow, inx, INF); )
+            for (vi inx(V, 0); F incr = augment(s, t, level, flow, inx, inf); )
                 res += incr;
         }
-        return res;
+        return make_tuple(res, flow);
     }
 };
 
