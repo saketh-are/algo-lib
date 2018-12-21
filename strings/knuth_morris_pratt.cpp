9e0eec6eb400b20096f3a87dcf3a06d8756927fb
Saketh Are <saketh.are@gmail.com>
2018-12-21 18:04:33 -0500

    kmp: add documentation

--- strings/knuth_morris_pratt.cpp
+++ strings/knuth_morris_pratt.cpp
@@ -9,12 +9,22 @@ template<typename T> struct kmp {
         return len + int(word[len] == nxt);
     }
 
+    /*
+     * Initializes the "failure table" for the search word.
+     * For i > 0, fail[i] is the length of the longest proper
+     * suffix of word[0, i) that is a prefix of word.
+     */
     kmp(const vector<T>& word) : P(word.size()), word(word) {
         fail.resize(P + 1);
         for (int i = 2; i <= P; i++)
             fail[i] = adv(fail[i - 1], word[i - 1]);
     }
 
+    /*
+     * Finds all occurences of the search word in the given text.
+     * match[i] indicates whether there exists a match starting at
+     * index i of text.
+     */
     vb find(const vector<T>& text) const {
         vb match(text.size());
         for (int i = 0, len = 0; i < text.size(); i++) {
