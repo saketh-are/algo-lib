156327a709e1f189612abd92a25672f14dd3ec20
Saketh Are <saketh.are@gmail.com>
2019-01-29 17:21:53 -0500

    segment_tree_lazy: add push_down flag

--- data_structures/segment_tree_lazy.cpp
+++ data_structures/segment_tree_lazy.cpp
@@ -1,27 +1,24 @@
 template<typename T, typename U> struct seg_tree_lazy {
     int S, H;
-
-    T zero;
     vector<T> value;
-
-    U noop;
-    vb dirty;
     vector<U> prop;
+    vb dirty;
+    bool push_down;
 
-    seg_tree_lazy<T, U>(int _S, T _zero = T(), U _noop = U()) {
-        zero = _zero, noop = _noop;
-        for (S = 1, H = 1; S < _S; ) S *= 2, H++;
-
-        value.resize(2*S, zero);
-        dirty.resize(2*S, false);
-        prop.resize(2*S, noop);
+    seg_tree_lazy<T, U>(int _S, bool push_down = true) : push_down(push_down) {
+        for (S = 1, H = 1; S < _S; S *= 2, H++);
+        value.resize(2*S), dirty.resize(2*S), prop.resize(2*S);
     }
 
-    void set_leaves(vector<T> &leaves) {
-        copy(leaves.begin(), leaves.end(), value.begin() + S);
-
+    void rebuild() {
         for (int i = S - 1; i > 0; i--)
-            value[i] = value[2 * i] + value[2 * i + 1];
+            value[i] = prop[i](value[2 * i] + value[2 * i + 1]);
+    }
+
+    void rebuild(int i) {
+        for (int l = i/2; l; l /= 2) {
+            value[l] = prop[l](value[2*l] + value[2*l+1]);
+        }
     }
 
     void apply(int i, U &update) {
@@ -32,22 +29,13 @@ template<typename T, typename U> struct seg_tree_lazy {
         }
     }
 
-    void rebuild(int i) {
-        for (int l = i/2; l; l /= 2) {
-            T combined = value[2*l] + value[2*l+1];
-            value[l] = prop[l](combined);
-        }
-    }
-
     void propagate(int i) {
         for (int h = H; h > 0; h--) {
             int l = i >> h;
-
             if (dirty[l]) {
                 apply(2*l, prop[l]);
                 apply(2*l+1, prop[l]);
-
-                prop[l] = noop;
+                prop[l] = U();
                 dirty[l] = false;
             }
         }
@@ -55,21 +43,20 @@ template<typename T, typename U> struct seg_tree_lazy {
 
     void upd(int i, int j, U update) {
         i += S, j += S;
-        propagate(i), propagate(j);
+        if (push_down) propagate(i), propagate(j);
 
         for (int l = i, r = j; l <= r; l /= 2, r /= 2) {
             if ((l&1) == 1) apply(l++, update);
             if ((r&1) == 0) apply(r--, update);
         }
-
         rebuild(i), rebuild(j);
     }
 
     T query(int i, int j) {
         i += S, j += S;
-        propagate(i), propagate(j);
+        if (push_down) propagate(i), propagate(j);
 
-        T res_left = zero, res_right = zero;
+        T res_left, res_right;
         for (; i <= j; i /= 2, j /= 2) {
             if ((i&1) == 1) res_left = res_left + value[i++];
             if ((j&1) == 0) res_right = value[j--] + res_right;
