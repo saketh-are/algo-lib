33e46eb584aa353c36d02667e0a61406ae0c7d1c
Eduardo Ferreira <edufgferreira@gmail.com>
2019-02-20 17:38:51 -0300

    Not now jek

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,6 +1,12 @@
+#include <stdexcept>
+
 namespace nithin_michael {
     int sqrt(double x) {
         return x + x;
+    }
+    
+    int jek_time() {
+        throw std::runtime_error("not now jek!");        
     }
 };
 
