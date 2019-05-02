0e653b9f3bf416c658fb27a03ca4906f9364c513
Saketh Are <saketh.are@gmail.com>
2019-05-02 14:33:51 -0400

    tarjan: avoid std::function memory overhead

--- graphs/strongly_connected_components.cpp
+++ graphs/strongly_connected_components.cpp
@@ -3,11 +3,11 @@ tuple<int, vi> strongly_connected_components(const vvi& graph) {
     int C = 0, V = 0, top = 0;
     vi comp(N, -1), inx(N, -1), stack(N + 1, -1);
 
-    function<int(int)> tarjan = [&](int loc) {
+    auto tarjan = [&](auto&& self, int loc) -> int {
         stack[top++] = loc;
         int low = inx[loc] = V++;
         for (int nbr : graph[loc]) {
-            if (inx[nbr] == -1) low = min(low, tarjan(nbr));
+            if (inx[nbr] == -1) low = min(low, self(self, nbr));
             else if (comp[nbr] == -1) low = min(low, inx[nbr]);
         }
         if (low == inx[loc]) {
@@ -18,9 +18,9 @@ tuple<int, vi> strongly_connected_components(const vvi& graph) {
         return low;
     };
 
-    for (int i = 0; i < graph.size(); i++) {
+    for (int i = 0; i < N; i++) {
         if (inx[i] == -1)
-            tarjan(i);
+            tarjan(tarjan, i);
     }
     return { C, comp };
 }
