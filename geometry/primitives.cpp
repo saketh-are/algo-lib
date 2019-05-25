b1c2a3c826e6b069b253e07009d654c5098b3e97
Saketh Are <saketh.are@gmail.com>
2019-05-24 23:20:15 -0400

    geometry: add primitives

---
+++ geometry/primitives.cpp
@@ -0,0 +1,122 @@
+template<typename T> struct point {
+    T x, y;
+    point() : x(0), y(0) {}
+    point(T x, T y) : x(x), y(y) {}
+    friend istream& operator >> (istream& i, point& p) { return i >> p.x >> p.y; }
+    friend ostream& operator << (ostream& o, const point& p) {
+        return o << "(" << p.x << ", " << p.y << ")";
+    }
+
+    T norm() const { return x * x + y * y; }
+    double len() const { return sqrt(norm()); }
+    double ang() const { return atan2(y, x); }
+    point<T> conj() const { return { x, -y }; }
+
+    point& operator += (const point& o) { x += o.x; y += o.y; return *this; }
+    point& operator -= (const point& o) { x -= o.x; y -= o.y; return *this; }
+    point& operator *= (const T& c) { x *= c; y *= c; return *this; }
+    point& operator /= (const T& c) { x /= c; y /= c; return *this; }
+    friend point operator + (const point& a, const point& b) { return point(a) += b; }
+    friend point operator - (const point& a, const point& b) { return point(a) -= b; }
+    friend point operator * (const point& a, const T& c) { return point(a) *= c; }
+    friend point operator * (const T& c, const point& a) { return point(a) *= c; }
+    friend point operator / (const point& a, const T& c) { return point(a) /= c; }
+
+    bool operator == (const point& o) const { return x == o.x && y == o.y; }
+    bool operator != (const point& o) const { return x != o.x || y != o.y; }
+    bool operator < (const point& o) const { return (x != o.x) ? x < o.x : y < o.y; }
+    bool operator > (const point& o) const { return (x != o.x) ? x > o.x : y > o.y; }
+    bool operator <= (const point& o) const { return !(*this > o); }
+
+    friend T dot(const point& a, const point& b) { return a.x * b.x + a.y * b.y; }
+    friend T cross(const point& a, const point& b) { return a.x * b.y - a.y * b.x; }
+    friend T cross(const point& p, const point& a, const point& b) { return cross(a - p, b - p); }
+
+    friend point<T> operator * (const point& a, const point& b) {
+        return { a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x };
+    }
+    friend point<T> operator / (const point& a, const point& b) {
+        return a * b.conj() / b.norm();
+    }
+
+    point dir() const { return point(*this) / len(); }
+    point rotate(const double& ang) const { return *this * point(cos(ang), sin(ang)); }
+
+    T dist(const point& a) const { return (a - *this).len(); }
+    T dist(const point& a, const point& b) {
+        return abs(cross(*this, a, b)) / a.dist(b);
+    }
+    double ang(const point& p, const point& r) const {
+        auto a = p - *this, b = r - *this;
+        double c = dot(a, b) / (a.len() * b.len());
+        return acos(max(-1., min(1., c)));
+    }
+    point reflect(const point& a, const point& b) const {
+        return a + ((*this - a) / (b - a)).conj() * (b - a);
+    }
+    point foot(const point& a, const point& b) const {
+        return (*this + this->reflect(a, b)) / 2;
+    }
+    friend point extension(const point& a, const point& b, const point& c, const point& d) {
+        T x = cross(a, b, c), y = cross(a, b, d);
+        return (d * x - c * y) / (x - y);
+    }
+
+    friend vector<point> seg_intersect(point a, point b, point c, point d) {
+        if (a > b) swap(a, b);
+        if (c > d) swap(c, d);
+
+        T a1 = cross(a, b, c), a2 = cross(a, b, d);
+        if (a1 > a2) swap(a1, a2);
+        if (!(a1 <= 0 && a2 >= 0)) return {};
+
+        if (a1 == 0 && a2 == 0) {
+            if (cross(a, c, d) != 0) return {};
+            point<T> x1 = max(a, c), x2 = min(b, d);
+            if (x1 > x2) return {};
+            if (x1 == x2) return { x1 };
+            return { x1, x2 };
+        }
+
+        point<T> z = extension(a, b, c, d);
+        if (a <= z && z <= b) return { z };
+        return {};
+    }
+
+    using polygon = vector<point<T>>;
+    friend ostream& operator << (ostream& o, const polygon& poly) {
+        o << "{";
+        for (auto pt : poly) o << " " << pt;
+        return o << " }";
+    }
+
+    string classify(const polygon& p) const {
+        bool ans = 0;
+        for (int i = 0; i < p.size(); i++) {
+            point<T> a = p[i], b = p[(i + 1) % p.size()];
+            if (cross(a, b, *this) == 0 && min(a, b) <= *this && *this <= max(a, b))
+                return "on";
+            if (a.y > b.y) swap(a, b);
+            if (a.y <= y && y < b.y && cross(*this, a, b) > 0) ans ^= 1;
+        }
+        return ans ? "in" : "out";
+    }
+
+    friend polygon convex_hull(const vector<point>& pts) {
+        point pivot = *min_element(all(pts));
+        auto sorted = pts;
+        sort(all(sorted), [&pivot](const auto& p, const auto& q) {
+            T cp = cross(pivot, p, q);
+            if (cp != 0) return cp > 0;
+            return pivot.dist(p) < pivot.dist(q);
+        });
+
+        polygon res;
+        for (const auto& p : sorted) {
+            while (res.size() >= 2 && cross(*(res.end() - 2), *(res.end() - 1), p) <= 0)
+                res.pop_back();
+            res.push_back(p);
+        }
+        return res;
+    }
+};
