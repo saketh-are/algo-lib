500b00f2065a608176af57420257a27ba343e62d
Saketh Are <saketh.are@gmail.com>
2019-06-11 18:24:48 -0400

    template: default to 64-bit mt19937

--- template.cpp
+++ template.cpp
@@ -17,7 +17,7 @@ using vvll = vector<vll>;
 using vb = vector<bool>;
 using vs = vector<string>;
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 int main() {
     ios_base::sync_with_stdio(false);
