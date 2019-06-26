c6de3f6bf0e9469aac37b8666fcef35e8af212e1
Saketh Are <saketh.are@gmail.com>
2019-06-26 11:27:11 -0400

    simpler print helper for tuples

--- template.cpp
+++ template.cpp
@@ -6,6 +6,11 @@ using namespace std;
 #define WATCHR(a, b) TRACE(for (auto it=a; it!=b;) cout << *(it++) << " "; cout << endl)
 #define WATCHC(V) TRACE({cout << #V" = "; WATCHR(V.begin(), V.end());})
 
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+
 #define sz(x) int((x).size())
 #define all(x) (x).begin(), (x).end()
 
@@ -21,29 +26,29 @@ using vs = vector<string>;
 template<typename T> void ckmin(T& a, const T& b) { a = min(a, b); }
 template<typename T> void ckmax(T& a, const T& b) { a = max(a, b); }
 
-template<class T> void print(ostream& o, T&& arg) { o << arg; }
-template<class T, class ...Ts> void print(ostream& o, T&& first, Ts&&... rest) {
-    o << first << ", "; print(o, forward<Ts&&>(rest)...);
-}
-template<class T, size_t... I>
-void print(ostream& o, const T& t, index_sequence<I...>) {
-    o << '['; print(o, get<I>(t)...); o << ']';
-};
-template<class ...T> ostream& operator<<(ostream& o, const tuple<T...>& t) {
-    print(o, t, make_index_sequence<sizeof...(T)>{}); return o;
-};
-template<typename T1, typename T2>
-ostream& operator<<(ostream& o, const pair<T1, T2>& t) {
-    return o << "[" << t.first << ", " << t.second << "]";
-};
-template<typename T, size_t N>
-ostream& operator<<(ostream& o, const array<T, N>& a) {
-    o << '[';
-    for (int i = 0; i < N; i++) { if (i) o << ", "; o << a[i]; }
-    return o << ']';
-};
-
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+namespace output {
+    template<typename T, size_t... I> void print_tuple(ostream& o,
+            const T& t, index_sequence<I...>) {
+        o << "("; ((o << (I ? ", " : "") << get<I>(t)), ...); o << ")";
+    }
+    template<typename... T> ostream& operator<<(ostream& o,
+            const tuple<T...>& t) {
+        print_tuple(o, t, index_sequence_for<T...>{}); return o;
+    }
+    template<typename T1, typename T2> ostream& operator<<(ostream& o,
+            const pair<T1, T2>& t) {
+        return o << "(" << t.first << ", " << t.second << ")";
+    }
+    template<typename T, size_t N> ostream& operator<<(ostream& o,
+            const array<T, N>& a) {
+        o << "(";
+        for (int i = 0; i < N; i++) { if (i) o << ", "; o << a[i]; }
+        return o << ")";
+    }
+}
+using namespace output;
 
 int main() {
     ios_base::sync_with_stdio(false);
