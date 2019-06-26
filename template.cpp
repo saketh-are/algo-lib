9eb71292723f791f1060bfd85b7cee9e5a1c35ca
Saketh Are <saketh.are@gmail.com>
2019-06-26 01:39:08 -0400

    add ckmin, ckmax, ostream helpers to template

--- template.cpp
+++ template.cpp
@@ -15,7 +15,33 @@ using vvi = vector<vi>;
 using vll = vector<ll>;
 using vvll = vector<vll>;
 using vb = vector<bool>;
+using vd = vector<double>;
 using vs = vector<string>;
+
+template<typename T> void ckmin(T& a, const T& b) { a = min(a, b); }
+template<typename T> void ckmax(T& a, const T& b) { a = max(a, b); }
+
+template<class T> void print(ostream& o, T&& arg) { o << arg; }
+template<class T, class ...Ts> void print(ostream& o, T&& first, Ts&&... rest) {
+    o << first << ", "; print(o, forward<Ts&&>(rest)...);
+}
+template<class T, size_t... I>
+void print(ostream& o, const T& t, index_sequence<I...>) {
+    o << '['; print(o, get<I>(t)...); o << ']';
+};
+template<class ...T> ostream& operator<<(ostream& o, const tuple<T...>& t) {
+    print(o, t, make_index_sequence<sizeof...(T)>{}); return o;
+};
+template<typename T1, typename T2>
+ostream& operator<<(ostream& o, const pair<T1, T2>& t) {
+    return o << "[" << t.first << ", " << t.second << "]";
+};
+template<typename T, size_t N>
+ostream& operator<<(ostream& o, const array<T, N>& a) {
+    o << '[';
+    for (int i = 0; i < N; i++) { if (i) o << ", "; o << a[i]; }
+    return o << ']';
+};
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
@@ -25,3 +51,4 @@ int main() {
 
     return 0;
 }
+
