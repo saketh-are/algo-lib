ff077f159fe875676001ff2f763b1967310363a7
Saketh Are <saketh.are@gmail.com>
2019-06-26 00:29:59 -0400

    add persistent union find

---
+++ data_structures/persistent_union_find.cpp
@@ -0,0 +1,51 @@
+struct persistent_union_find {
+    struct node {
+        int par, rank;
+        node(int id = 0) : par(id), rank(0) {}
+        void operator +=(const node& o) {
+            if (rank == o.rank) rank++;
+        }
+    };
+
+    int N, ops;
+    vector<node> dsu;
+    vector<tuple<int, node, int, int>> history;
+
+    persistent_union_find(int _N) : N(_N), ops(0) {
+        for (int i = 0; i < N; i++)
+            dsu.push_back(node(i));
+    }
+
+    int opcount() const { return ops; }
+    int components() const { return N - sz(history); }
+
+    int rep(int i) const {
+        while (i != dsu[i].par)
+            i = dsu[i].par;
+        return i;
+    }
+
+    bool unio(int a, int b) {
+        ops++;
+        a = rep(a), b = rep(b);
+        if(a == b) return false;
+
+        if (dsu[a].rank < dsu[b].rank)
+            swap(a, b);
+
+        history.emplace_back(ops, dsu[a], a, b);
+        dsu[a] += dsu[b];
+        dsu[b].par = a;
+
+        return true;
+    }
+
+    void rewind(int _ops) {
+        for (ops = _ops; !history.empty(); history.pop_back()) {
+            const auto &[op, av, a, b] = history.back();
+            if (op <= ops) break;
+            dsu[a] = av;
+            dsu[b].par = b;
+        }
+    }
+};
