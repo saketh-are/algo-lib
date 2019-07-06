72c0d6c8fe9ac0a4b490103be14e98d728544bda
Saketh Are <saketh.are@gmail.com>
2019-07-05 20:56:35 -0400

    fft with modnum support, better usability

--- numeric/fft.cpp
+++ numeric/fft.cpp
@@ -1,69 +1,97 @@
-template<typename T> struct FFT {
-    int deg;
-    vi rev;
+namespace FFT {
+    using dbl = double;
+    struct cnum : point<dbl> {
+        cnum(point<dbl> p) : point<dbl>(p) {}
+        cnum(dbl _x = 0, dbl _y = 0) : point<dbl>(_x, _y) {}
+        cnum inv() const { dbl n = norm(); return cnum(x / n, -y / n); }
 
-    T deg_inv;
-
-    FFT(int _deg, T _deg_inv) {
-        assert(__builtin_popcount(_deg) == 1);
-        deg = _deg;
-        deg_inv = _deg_inv;
-        rev.resize(deg);
-
-        for (int i = 1, j = 0; i < deg; i++) {
-            int bit = deg / 2;
-            for (; j >= bit; bit /= 2)
-                j -= bit;
-            j += bit;
-            rev[i] = j;
+        static cnum unity_root(int deg, int pow) {
+            static vector<cnum> table{ 0, 1 };
+            while (sz(table) <= deg) {
+                dbl t = 2 * M_PI / sz(table);
+                cnum w{ cos(t), sin(t) };
+                for (int i = sz(table)/2, s = sz(table); i < s; i++) {
+                    table.push_back(table[i]);
+                    table.push_back(table[i] * w);
+                }
+            }
+            return table[deg + (pow < 0 ? deg + pow : pow)];
         }
-    }
+    };
 
-    void transform(vector<T> &poly, bool invert) {
-        assert(poly.size() == deg);
+    template<typename T, bool INV> static void fft(vector<T>& a, int N) {
+        assert(__builtin_popcount(N) == 1);
 
-        for (int i = 1; i < deg; i++)
-            if(rev[i] > i)
-                swap(poly[i], poly[rev[i]]);
+        for (int i = 1, j = 0, k; i < N; i++) {
+            for (k = N >> 1; (j ^= k) < k; k >>= 1);
+            if (i < j) swap(a[i], a[j]);
+        }
 
-        for (int len = 2; len <= deg; len *= 2) {
-            T base = T::unity_root(deg, deg/len * (invert ? -1 : 1));
-
-            for (int i = 0; i < deg; i += len) {
-                T w = T::one();
-
-                for (int j = 0; j < len / 2; j++) {
-                    T u = poly[i+j];
-                    T v = w * poly[i+j+len/2];
-
-                    poly[i+j] = u + v;
-                    poly[i+j+len/2] = u - v;
-
-                    w = w * base;
+        for (int l = 1; l < N; l <<= 1) {
+            for (int i = 0; i < N; i += 2 * l) {
+                for (int j = 0; j < l; j++) {
+                    T w = T::unity_root(2 * l, INV ? -j : j);
+                    T u = a[i + j];
+                    T v = a[i + j + l] * w;
+                    a[i + j] = u + v;
+                    a[i + j + l] = u - v;
                 }
             }
         }
+        if (INV) for (T& t : a) t = t * T(N).inv();
+    }
 
-        if(invert) {
-            for (int i = 0; i < deg; i++)
-                poly[i] = poly[i] * deg_inv;
+    static vector<cnum> fa, fb;
+
+    vector<cnum> operator*(const vector<cnum>& a, const vector<cnum>& b) {
+        if (a.empty() || b.empty()) return {};
+        int s = sz(a) + sz(b) - 1;
+        int N = 1 << (s > 1 ? 32 - __builtin_clz(s - 1) : 0);
+        if (N > sz(fa)) fa.resize(N), fb.resize(N);
+
+        copy(all(a), fa.begin()), copy(all(b), fb.begin());
+        fill(fa.begin() + sz(a), fa.begin() + N, 0);
+        fill(fb.begin() + sz(b), fb.begin() + N, 0);
+
+        fft<cnum, false>(fa, N), fft<cnum, false>(fb, N);
+        for (int i = 0; i < N; i++) fa[i] = fa[i] * fb[i];
+        fft<cnum, true>(fa, N);
+        return { fa.begin(), fa.begin() + s };
+    }
+
+    vector<modnum<MOD>> operator*(const vector<modnum<MOD>>& a, const vector<modnum<MOD>>& b) {
+        if (a.empty() || b.empty()) return {};
+        int s = sz(a) + sz(b) - 1;
+        int N = 1 << (s > 1 ? 32 - __builtin_clz(s - 1) : 0);
+        if (N > sz(fa)) fa.resize(N), fb.resize(N);
+
+        for (int i = 0; i < sz(a); i++) fa[i] = cnum(a[i].v & ((1 << 15) - 1), a[i].v >> 15);
+        fill(fa.begin() + sz(a), fa.begin() + N, 0);
+        for (int i = 0; i < sz(b); i++) fb[i] = cnum(b[i].v & ((1 << 15) - 1), b[i].v >> 15);
+        fill(fb.begin() + sz(b), fb.begin() + N, 0);
+
+        fft<cnum, false>(fa, N), fft<cnum, false>(fb, N);
+        for (int i = 0; i <= N / 2; i++) {
+            int j = (N - i) & (N - 1);
+            cnum g0 = (fb[i] + fb[j].conj()) / (2 * N);
+            cnum g1 = (fb[i] - fb[j].conj()) / (2 * N);
+            g1 = { g1.y, -g1.x };
+            if (j != i) {
+                swap(fa[j], fa[i]);
+                fb[j] = fa[j] * g1;
+                fa[j] = fa[j] * g0;
+            }
+            fb[i] = fa[i] * g1.conj();
+            fa[i] = fa[i] * g0.conj();
         }
+        fft<cnum, false>(fa, N), fft<cnum, false>(fb, N);
+
+        vector<modnum<MOD>> c(s);
+        for (int i = 0; i < s; i++)
+            c[i] =  ll(fa[i].x + 0.5)
+                 + (ll(fa[i].y + 0.5) % MOD << 15)
+                 + (ll(fb[i].x + 0.5) % MOD << 15)
+                 + (ll(fb[i].y + 0.5) % MOD << 30);
+        return c;
     }
-};
-
-struct cnum {
-    static constexpr double PI = 2 * atan2(1, 0);
-    static constexpr cnum one() { return { 1, 0 }; }
-
-    double a, b;
-    cnum operator+(const cnum &c) { return { a + c.a, b + c.b }; }
-    cnum operator-(const cnum &c) { return { a - c.a, b - c.b }; }
-    cnum operator*(const cnum &c) { return { a*c.a - b*c.b, a*c.b + b*c.a }; }
-
-    // returns x^pow for x such that x^deg = 1, x^(deg-1) != 1
-    static cnum unity_root(int deg, int pow) {
-        double ang = pow * 2 * PI / deg;
-        return { cos(ang), sin(ang) };
-    }
-};
-
+}
