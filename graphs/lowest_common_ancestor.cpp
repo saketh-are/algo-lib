2eca21f9079723227c6f10afe45e4a7e3fa95117
Saketh Are <saketh.are@gmail.com>
2019-07-26 20:08:55 -0400

    lca: add 'first_step' function

--- graphs/lowest_common_ancestor.cpp
+++ graphs/lowest_common_ancestor.cpp
@@ -3,43 +3,51 @@
  */
 struct lowest_common_ancestor {
     struct visit {
-        int node, depth;
-        visit operator + (const visit& o) const {
-            return depth <= o.depth ? *this : o;
+        int node, depth, index;
+        visit operator +(const visit& o) const {
+            if (depth != o.depth) return depth < o.depth ? *this : o;
+            return index > o.index ? *this : o;
         }
     };
 
-    vi depth, pos, comp;
-    sparse_table<visit> euler;
+    int V;
+    vector<visit> tour;
+    sparse_table<visit> table;
+    vi first, depth, par, comp;
 
-    lowest_common_ancestor(const vvi& tree = {}, int root = -1) {
-        depth.resize(sz(tree));
-        comp.resize(sz(tree), -1);
-        pos.resize(sz(tree), -1);
+    lowest_common_ancestor(const vvi& tree = {}, vi roots = {}) : V(sz(tree)) {
+        depth.resz(V), comp.resz(V, -1), first.resz(V, -1), par.resz(V, -1);
 
-        vector<visit> tour; int cc = 0;
-        auto dfs = [&](auto& self, int loc, int par) -> void {
-            comp[loc] = cc, pos[loc] = sz(tour);
-            tour.push_back({ loc, depth[loc] });
-            for (int nbr : tree[loc]) if (nbr != par) {
+        int cc = 0;
+        auto dfs = [&](auto& self, int loc) -> void {
+            comp[loc] = cc, first[loc] = sz(tour);
+            tour.push_back({loc, depth[loc], sz(tour)});
+            for (int nbr : tree[loc]) if (nbr != par[loc]) {
+                par[nbr] = loc;
                 depth[nbr] = depth[loc] + 1;
-                self(self, nbr, loc);
-                tour.push_back({ loc, depth[loc] });
+                self(self, nbr);
+                tour.push_back({loc, depth[loc], sz(tour)});
             }
         };
-        if (root != -1) dfs(dfs, root, root), cc++;
-        for (int i = 0; i < sz(tree); i++)
-            if (comp[i] == -1) dfs(dfs, i, i), cc++;
-        euler = sparse_table<visit>(tour);
+        for (int i : roots) {
+            assert(comp[i] == -1);
+            dfs(dfs, i), cc++;
+        }
+        for (int i = 0; i < V; i++) {
+            if (comp[i] == -1)
+                dfs(dfs, i), cc++;
+        }
+
+        table = sparse_table<visit>(tour);
     }
 
     // Returns the lowest common ancestor of u and v
     // Returns -1 if they are in different components
     int lca(int u, int v) {
         if (comp[u] != comp[v]) return -1;
-        u = pos[u], v = pos[v];
+        u = first[u], v = first[v];
         if (u > v) swap(u, v);
-        return euler(u, v + 1).node;
+        return table(u, v + 1).node;
     }
 
     int dist(int u, int v) {
@@ -50,5 +58,12 @@ struct lowest_common_ancestor {
     // Checks if btw is on the path from u to v
     bool on_path(int u, int v, int btw) {
         return dist(u, v) == (dist(u, btw) + dist(btw, v));
+    }
+
+    // Returns the neighbor of u on the shortest path to v
+    int first_step(int u, int v) {
+        assert(u != v && comp[u] == comp[v]);
+        int w = lca(u, v);
+        return u == w ? tour[table(first[u], first[v]).index+1].node : par[u];
     }
 };
