20a0ecb6be9391f96a06bca2b4bc4887aa1c5d7f
Saketh Are <saketh.are@gmail.com>
2019-08-28 15:39:49 -0400

    update style of persistent_segment_tree

--- data_structures/persistent_segment_tree.cpp
+++ data_structures/persistent_segment_tree.cpp
@@ -1,49 +1,37 @@
-template<typename T> struct pseg_tree {
-    T zero;
+template<typename T, typename I> struct persistent_segment_tree {
+    using F = function<T(const T&, const T&)>;
 
-    int S;
-    vector<vector<pair<int, T>>> value;
+    size_t SZ;
+    F combine;
+    vector<vector<pair<I, T>>> table;
 
-    pseg_tree<T>(int _S, T _zero = T()) : S(_S), zero(_zero) {
-        value.resize(2*S+1);
+    T get(int i) const { return sz(table[i]) ? table[i].back().s : T{}; }
+    T get(int i, I w) const {
+        static auto cmp = [](const pair<I, T>& a, const pair<I, T>& b) { return a.f < b.f; };
+        auto it = ub(all(table[i]), mp(w, T{}), cmp);
+        return it != table[i].begin() ? prev(it)->s : T{};
     }
 
-    void recompute(int op, int i) {
-        const T& c1 = value[2 * i].empty() ? zero : value[2 * i].back().second;
-        const T& c2 = value[2 * i + 1].empty() ? zero : value[2 * i + 1].back().second;
-        value[i].emplace_back(op, c1 + c2);
+    persistent_segment_tree<T, I>(size_t _SZ, F combine) : SZ(_SZ), combine(combine) {
+        table.resize(2 * SZ);
+        FORd (i, SZ, sz(table)) table[i].reserve(1);
+        FORd (i, 1, SZ) table[i].reserve(table[2*i].capacity() + table[2*i+1].capacity());
     }
 
-    void set_leaves(vector<T>& leaves) {
-        for (int i = S; i < value.size(); i++)
-            value[i].emplace_back(0, leaves[i]);
-        for (int i = S - 1; i > 0; i--)
-            recompute(0, i);
+    void replace(int i, T v, I w) {
+        table[i += SZ].eb(w, v);
+        for (i /= 2; i; i /= 2)
+            table[i].eb(w, combine(get(2 * i), get(2 * i + 1)));
     }
 
-    void upd(int i, T v, int op) {
-        i += S;
-        value[i].emplace_back(op, v);
-        while(i>1){
-            i/=2;
-            recompute(op, i);
+    T operator[](int i) const { return get(SZ + i); }
+
+    T operator()(int i, int j, I w) const {
+        T left{}, right{};
+        for (i += SZ, j += SZ; i < j; i /= 2, j /= 2) {
+            if (i&1) left = combine(left, get(i++, w));
+            if (j&1) right = combine(get(--j, w), right);
         }
-    }
-
-    T find(vector<pair<int, T>>& hist, int op) {
-        auto it = upper_bound(all(hist), make_pair(op, zero ), [](auto& a, auto &b) {
-            return a.first < b.first;
-        });
-        return it != hist.begin() ? prev(it)->second : zero;
-    }
-
-    T query(int i, int j, int op) {
-        T res_left = zero, res_right = zero;
-        for(i += S, j += S; i <= j; i /= 2, j /= 2){
-            if((i&1) == 1) res_left = res_left + find(value[i++], op);
-            if((j&1) == 0) res_right = find(value[j--], op) + res_right;
-        }
-        return res_left + res_right;
+        return combine(left, right);
     }
 };
-
