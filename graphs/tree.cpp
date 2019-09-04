b1e9aa71661379a330180242b1ad4983f7fc7f92
Saketh Are <saketh.are@gmail.com>
2019-09-03 20:30:27 -0400

    tree: add subtree size and centroid functions

--- graphs/tree.cpp
+++ graphs/tree.cpp
@@ -1,31 +1,52 @@
 template<typename E> struct tree {
     int V;
-    vvi nbrs, eids;
+    vvi nbrs;
     vector<E> edges;
+    vector<vector<E>> adj;
+
+    vi subt_sz;
+    vb erased;
 
     tree() {}
     tree(int _V) : V(_V) {
-        nbrs.resz(V), eids.resz(V), edges.resz(V-1);
+        edges.resz(V-1);
+        nbrs.resz(V), adj.resz(V), subt_sz.resz(V), erased.resz(V);
     }
-    tree(const vector<E>& __edges) : V(sz(__edges) + 1), edges(__edges)  {
-        nbrs.resz(V), eids.resz(V);
+    tree(const vector<E>& __edges) : tree(sz(__edges) + 1) {
+        edges = __edges;
         init();
     }
 
     void init() {
         F0R (i, sz(edges)) {
-            auto& e = edges[i];
+            auto& e = edges[i]; e.i = i;
             assert(0 <= e.u && e.u < V && 0 <= e.v && e.v < V);
-
-            nbrs[e.u].pb(e.v);
-            nbrs[e.v].pb(e.u);
-
-            e.i = i;
-            eids[e.u].pb(i);
-            eids[e.v].pb(i);
+            adj[e.u].pb(e), nbrs[e.u].pb(e.v);
+            adj[e.v].pb(e), nbrs[e.v].pb(e.u);
         }
     }
 
     friend void re(tree& t) { re(t.edges); t.init(); }
     friend void pr(const tree& t) { pr("{V=", t.V, " ", t.edges, "}"); }
+
+    void erase(int v) { erased[v] = true; }
+
+    void calc_subt_sz(int v, int p = -1) {
+        subt_sz[v] = 1;
+        trav (e, adj[v]) if (int u = e[v]; !erased[u] && u != p) {
+            calc_subt_sz(u, v);
+            subt_sz[v] += subt_sz[u];
+        }
+    }
+
+    int centroid(int v) {
+        calc_subt_sz(v);
+        int c = v;
+        FIND: trav (e, adj[c]) if (int u = e[c]; !erased[u]) {
+            if (subt_sz[u] < subt_sz[c] && 2 * subt_sz[u] >= subt_sz[v]) {
+                c = u; goto FIND;
+            }
+        }
+        return c;
+    }
 };
