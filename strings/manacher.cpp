f80145a1bc7b5b0691ce25dc06acbeb25547b112
Saketh Are <saketh.are@gmail.com>
2019-09-19 12:12:32 -0400

    manacher: cleaner templating

--- strings/manacher.cpp
+++ strings/manacher.cpp
@@ -5,7 +5,7 @@
  * For odd i,  pal[i] = the length of the longest palindrome centered
  *                      between indices i/2 and i/2+1 of the input string
  */
-template<typename T> vi manacher(const vector<T>& str) {
+template<typename T> vi manacher(T str) {
     vi pal(max(2 * int(str.size()) - 1, 0));
 
     auto match = [&str, &pal](int i, int l) {
@@ -13,7 +13,7 @@ template<typename T> vi manacher(const vector<T>& str) {
         return (i-l)&1 || str[(i-l)/2] == str[(i+l)/2];
     };
 
-    for (int c = 0, end = -1, ref; c < pal.size(); c++) {
+    for (int c = 0, end = -1, ref; c < sz(pal); c++) {
         if (end > c)
             pal[c] = min(pal[2 * ref - c], end - c);
         while (match(c, pal[c] + 1))
@@ -25,4 +25,3 @@ template<typename T> vi manacher(const vector<T>& str) {
     }
     return pal;
 }
-
