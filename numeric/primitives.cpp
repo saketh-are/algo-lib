d06c0aaaacb3d6c72ea16398af8ff5c5e2bb310e
Saketh Are <saketh.are@gmail.com>
2019-10-08 23:45:21 -0400

    sieve: compute mobius, tweak factorization

--- numeric/primitives.cpp
+++ numeric/primitives.cpp
@@ -1,22 +1,28 @@
 namespace number_theory {
     template<size_t N> struct sieve {
-        vi primes, least_prime, largest_proper, phi;
-        sieve() : least_prime(N + 1), largest_proper(N + 1), phi(N + 1) {
-            phi[1] = 1;
+        vi primes, least_prime, largest_proper, phi, mu;
+        sieve() : least_prime(N + 1), largest_proper(N + 1), phi(N + 1), mu(N + 1) {
             for (int i = 2; i <= N; i++) {
                 if (!least_prime[i]) {
                     least_prime[i] = i;
                     largest_proper[i] = 1;
                     primes.push_back(i);
                 }
-
-                phi[i] = phi[largest_proper[i]];
-                phi[i] *= least_prime[i] - int(least_prime[i] != least_prime[largest_proper[i]]);
-
                 for (int x : primes) {
                     if (x > least_prime[i] || i * x > N) break;
                     least_prime[i * x] = x;
                     largest_proper[i * x] = i;
+                }
+            }
+
+            phi[1] = mu[1] = 1;
+            for (int i = 2; i <= N; i++) {
+                if (least_prime[largest_proper[i]] == least_prime[i]) {
+                    phi[i] = phi[largest_proper[i]] * least_prime[i];
+                    mu[i] = 0;
+                } else {
+                    phi[i] = phi[largest_proper[i]] * (least_prime[i] - 1);
+                    mu[i] = -mu[largest_proper[i]];
                 }
             }
         }
@@ -26,12 +32,15 @@ namespace number_theory {
             return least_prime[v] == v;
         }
 
-        vi factor(int v) {
+        vpii factor(int v) {
             assert(0 < v && v <= N);
-            vi res;
+            vpii res;
             while (v > 1) {
-                res.push_back(least_prime[v]);
-                v = largest_proper[v];
+                res.emplace_back(least_prime[v], 0);
+                while (least_prime[v] == res.back().f) {
+                    v = largest_proper[v];
+                    res.back().s++;
+                }
             }
             reverse(all(res));
             return res;
