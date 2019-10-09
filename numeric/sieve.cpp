13a94c9005b9f72a3172d9f9e21daae57ee93611
Saketh Are <saketh.are@gmail.com>
2019-10-09 12:38:05 -0400

    refactor sieve

--- numeric/sieve.cpp
+++ numeric/sieve.cpp
@@ -1,60 +1,53 @@
 template<size_t N> struct sieve {
-    vi primes, least_prime, largest_proper, phi, mu;
-    sieve() : least_prime(N + 1), largest_proper(N + 1), phi(N + 1), mu(N + 1) {
-        for (int i = 2; i <= N; i++) {
-            if (!least_prime[i]) {
-                least_prime[i] = i;
-                largest_proper[i] = 1;
-                primes.push_back(i);
-            }
-            for (int x : primes) {
-                if (x > least_prime[i] || i * x > N) break;
-                least_prime[i * x] = x;
-                largest_proper[i * x] = i;
-            }
-        }
+    vi primes;
+    struct num {
+        int lp;   // least prime divisor
+        char lpk; // multiplicity of least prime divisor
+        int pd;   // largest proper divisor (num / lp)
+        int wlp;  // largest divisor without the least prime (num / (lp ** lpk))
+        int phi;  // euler's totient function
+        char mu;  // mobius function
+    };
+    vector<num> nums;
+    const num& operator[](int i){ return nums[i]; }
 
-        phi[1] = mu[1] = 1;
+    sieve() : nums(N + 1) {
+        nums[1] = { -1, 0, -1, -1, 1, 1 };
         for (int i = 2; i <= N; i++) {
-            if (least_prime[largest_proper[i]] == least_prime[i]) {
-                phi[i] = phi[largest_proper[i]] * least_prime[i];
-                mu[i] = 0;
-            } else {
-                phi[i] = phi[largest_proper[i]] * (least_prime[i] - 1);
-                mu[i] = -mu[largest_proper[i]];
+            num& n = nums[i];
+            if (!n.lp) {
+                n = { i, 1, 1, 1, i - 1, -1 };
+                primes.pb(i);
+            }
+            for (int p : primes) {
+                if (p > n.lp || p * i > N) break;
+                if (p < n.lp) nums[p * i] = { p, 1, i, i, n.phi * (p - 1), -n.mu };
+                else nums[p * i] = { p, n.lpk + 1, i, n.wlp, n.phi * p, 0 };
             }
         }
     }
 
     bool is_prime(int v) {
-        assert(0 < v && v < N);
-        return least_prime[v] == v;
+        assert(0 < v && v <= N);
+        return nums[v].lp == v;
     }
 
     vpii factor(int v) {
         assert(0 < v && v <= N);
         vpii res;
-        while (v > 1) {
-            res.emplace_back(least_prime[v], 0);
-            while (least_prime[v] == res.back().f) {
-                v = largest_proper[v];
-                res.back().s++;
-            }
-        }
+        for (; v > 1; v = nums[v].wlp)
+            res.emplace_back(nums[v].lp, nums[v].lpk);
         reverse(all(res));
         return res;
     }
 
     vi divisors(int v) {
+        assert(0 < v && v <= N);
         if (v == 1) return {1};
-
-        int p = least_prime[v], m = 0;
-        while (v % p == 0) v /= p, m++;
-
-        vi res = divisors(v), fin;
-        for (int d : res)
-            for (int pc = 0, t = d; pc <= m; pc++, t *= p)
-                fin.pb(t);
-        return fin;
+        vi res;
+        for (int d : divisors(nums[v].wlp))
+            for (int k = 0; k <= nums[v].lpk; k++, d *= nums[v].lp)
+                res.pb(d);
+        return res;
     }
 };
