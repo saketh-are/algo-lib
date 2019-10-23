2ffdf64f0d850f758a75f7e1b0653cb78262f128
Saketh Are <saketh.are@gmail.com>
2019-10-23 16:02:11 -0400

    modnum: alias primitive types in case of __int128

--- numeric/modnum.cpp
+++ numeric/modnum.cpp
@@ -1,9 +1,11 @@
-template<int MOD> struct modnum {
-    int v;
+using v_t = int;
+using vv_t = ll;
+template<v_t MOD> struct modnum {
+    v_t v;
     modnum() : v(0) {}
-    modnum(ll _v) : v(_v % MOD) { if (v < 0) v += MOD; }
-    explicit operator int() const { return v; }
-    friend istream& operator >> (istream& i, modnum& n) { ll w; i >> w; n = modnum(w); return i; }
+    modnum(vv_t _v) : v(_v % MOD) { if (v < 0) v += MOD; }
+    explicit operator v_t() const { return v; }
+    friend istream& operator >> (istream& i, modnum& n) { vv_t w; i >> w; n = modnum(w); return i; }
     friend ostream& operator << (ostream& o, const modnum& n) { return o << n.v; }
 
     friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
@@ -11,13 +13,13 @@ template<int MOD> struct modnum {
 
     modnum& operator += (const modnum& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
     modnum& operator -= (const modnum& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
-    modnum& operator *= (const modnum& o) { v = int(ll(v) * ll(o.v) % MOD); return *this; }
+    modnum& operator *= (const modnum& o) { v = v_t(vv_t(v) * o.v % MOD); return *this; }
     modnum operator - () { modnum res; if (v) res.v = MOD - v; return res; }
     friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
     friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
     friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
 
-    modnum pow(ll e) const {
+    modnum pow(vv_t e) const {
         if (e < 0) return 1 / this->pow(-e);
         if (e == 0) return 1;
         if (e & 1) return *this * this->pow(e-1);
@@ -25,9 +27,9 @@ template<int MOD> struct modnum {
     }
 
     modnum inv() const {
-        int g = MOD, x = 0, y = 1;
-        for (int r = v; r != 0; ) {
-            int q = g / r;
+        v_t g = MOD, x = 0, y = 1;
+        for (v_t r = v; r != 0; ) {
+            v_t q = g / r;
             g %= r; swap(g, r);
             x -= q * y; swap(x, y);
         }
@@ -39,9 +41,9 @@ template<int MOD> struct modnum {
     modnum& operator /= (const modnum& o) { return (*this) *= o.inv(); }
     friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= modnum(b); }
 
-    static constexpr int totient() {
-        int tot = MOD, tmp = MOD;
-        for (int p = 2; p * p <= tmp; p++) if (tmp % p == 0) {
+    static constexpr v_t totient() {
+        v_t tot = MOD, tmp = MOD;
+        for (v_t p = 2; p * p <= tmp; p++) if (tmp % p == 0) {
             tot = tot / p * (p - 1);
             while (tmp % p == 0) tmp /= p;
         }
@@ -49,37 +51,37 @@ template<int MOD> struct modnum {
         return tot;
     }
 
-    static int primitive_root() {
+    static v_t primitive_root() {
         if (MOD == 1) return 0;
         if (MOD == 2) return 1;
 
-        int tot = totient(), tmp = tot;
+        v_t tot = totient(), tmp = tot;
         vi tot_pr;
-        for (int p = 2; p * p <= tmp; p++) if (tot % p == 0) {
+        for (v_t p = 2; p * p <= tmp; p++) if (tot % p == 0) {
             tot_pr.push_back(p);
             while (tmp % p == 0) tmp /= p;
         }
         if (tmp > 1) tot_pr.push_back(tmp);
 
-        for (int r = 2; r < MOD; r++) if (__gcd(r, MOD) == 1) {
+        for (v_t r = 2; r < MOD; r++) if (__gcd(r, MOD) == 1) {
             bool root = true;
-            for (int p : tot_pr) root &= modnum(r).pow(tot / p) != 1;
+            for (v_t p : tot_pr) root &= modnum(r).pow(tot / p) != 1;
             if (root) return r;
         }
         assert(false);
     }
 
     static modnum generator() { static modnum g = primitive_root(); return g; }
-    static int discrete_log(modnum v) {
-        static const int M = ceil(sqrt(MOD));
-        static unordered_map<int, int> table;
+    static v_t discrete_log(modnum v) {
+        static const v_t M = ceil(sqrt(MOD));
+        static unordered_map<v_t, v_t> table;
         if (table.empty()) {
             modnum e = 1;
-            for (int i = 0; i < M; i++) { table[e.v] = i; e *= generator(); }
+            for (v_t i = 0; i < M; i++) { table[e.v] = i; e *= generator(); }
         }
         static modnum f = generator().pow(totient() - M);
 
-        for (int i = 0; i < M; i++) {
+        for (v_t i = 0; i < M; i++) {
             if (table.count(v.v)) return table[v.v] + i * M;
             v *= f;
         }
