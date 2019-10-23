f298fab84e4803159b485f8d54bf6e261bd5c52e
Saketh Are <saketh.are@gmail.com>
2019-10-23 14:03:46 -0400

    garner: include inverse implementation, return ll

--- numeric/garner.cpp
+++ numeric/garner.cpp
@@ -1,22 +1,35 @@
 /*
- * Garner's algorithm: finds the unique v modulo prod(MOD)
- * such that v = n[i] (MOD[i]) for all i.
- * Elements of MOD should be pairwise co-prime.
+ * Garner's algorithm: finds the unique v modulo prod(MODS)
+ * such that v is congruent to n[i] modulo MODS[i] for all i.
+ * Elements of MODS should be pairwise co-prime.
  */
-int crt(const vi& MOD, const vi& n) {
-    const int M = sz(MOD);
+ll crt(const vi& MODS, const vi& n) {
+    auto inverse = [](int M, int n) {
+        int g = M, x = 0, y = 1;
+        for (int r = n; r != 0; ) {
+            int q = g / r;
+            g %= r; swap(g, r);
+            x -= q * y; swap(x, y);
+        }
+
+        assert(g == 1);
+        assert(y == M || y == -M);
+        return x < 0 ? x + M : x;
+    };
+
+    const int M = sz(MODS);
 
     vi x(M);
     for (int i = 0; i < M; i++) {
         x[i] = n[i];
         for (int j = 0; j < i; j++) {
-            x[i] = ll(x[i] - x[j]) * inverse(MOD[i], MOD[j] % MOD[i]) % MOD[i];
-            if (x[i] < 0) x[i] += MOD[i];
+            x[i] = ll(x[i] - x[j]) * inverse(MODS[i], MODS[j] % MODS[i]) % MODS[i];
+            if (x[i] < 0) x[i] += MODS[i];
         }
     }
 
-    int v = 0;
+    ll v = 0;
     for (int i = M - 1; i >= 0; i--)
-        v = MOD[i] * v + x[i];
+        v = MODS[i] * v + x[i];
     return v;
 }
