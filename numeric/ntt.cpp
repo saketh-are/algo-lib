ae31a4ffcecae96ca9bd092dcf5722757322b8ee
Saketh Are <saketh.are@gmail.com>
2019-10-24 22:27:35 -0400

    ntt: perf optimizations, add pow

--- numeric/ntt.cpp
+++ numeric/ntt.cpp
@@ -1,39 +1,43 @@
 namespace ntt {
-    template<int MOD> struct mnum : modnum<MOD> {
-        mnum(modnum<MOD> m) : modnum<MOD>(m) {}
-        mnum(ll _v = 0) : modnum<MOD>(_v) {}
-
-        static mnum unity_root(int deg, int pow) {
-            static vector<mnum> table{ 0, 1 };
-            while (sz(table) <= deg) {
-                mnum w = modnum<MOD>::unity_root(sz(table));
-                for (int i = sz(table)/2, s = sz(table); i < s; i++) {
-                    table.push_back(table[i]);
-                    table.push_back(table[i] * w);
-                }
-            }
-            return table[deg + (pow < 0 ? deg + pow : pow)];
-        }
-    };
-
     template<int MOD>
-    vector<modnum<MOD>> operator*(const vector<modnum<MOD>>& a, const vector<modnum<MOD>>& b) {
+    vector<modnum<MOD>> operator*(vector<modnum<MOD>> a, vector<modnum<MOD>> b) {
         if (a.empty() || b.empty()) return {};
         int s = sz(a) + sz(b) - 1;
+        if (min(sz(a), sz(b)) < 150) {
+            const vv_t VV_BOUND = numeric_limits<vv_t>::max() - vv_t(MOD) * MOD;
+            vector<vv_t> res(s);
+            for (int i = 0; i < sz(a); i++) for (int j = 0; j < sz(b); j++) {
+                if ((res[i + j] += vv_t(a[i].v) * b[j].v) > VV_BOUND) res[i + j] %= MOD;
+            }
+            return {res.begin(), res.end()};
+        }
+
         int N = 1 << (s > 1 ? 32 - __builtin_clz(s - 1) : 0);
 
-        static vector<mnum<MOD>> fa, fb;
-        if (N > sz(fa)) fa.resize(N), fb.resize(N);
+        bool eq = a == b;
+        a.resz(N);
+        fft<modnum<MOD>, false>(a, N);
 
-        copy(all(a), fa.begin());
-        fill(fa.begin() + sz(a), fa.begin() + N, 0);
-        copy(all(b), fb.begin());
-        fill(fb.begin() + sz(b), fb.begin() + N, 0);
+        if (!eq) {
+            b.resz(N);
+            fft<modnum<MOD>, false>(b, N);
+            for (int i = 0; i < N; i++) a[i] *= b[i];
+        } else {
+            for (int i = 0; i < N; i++) a[i] *= a[i];
+        }
 
-        fft<mnum<MOD>, false>(fa, N);
-        fft<mnum<MOD>, false>(fb, N);
-        for (int i = 0; i < N; i++) fa[i] = fa[i] * fb[i];
-        fft<mnum<MOD>, true>(fa, N);
-        return { fa.begin(), fa.begin() + s };
+        fft<modnum<MOD>, true>(a, N);
+        a.resz(s);
+        return a;
+    }
+
+    template<int MOD> vector<modnum<MOD>> pow(vector<modnum<MOD>> v, int p) {
+        vector<modnum<MOD>> r = {1};
+        if (!p) return r;
+        for (int i = 31 - __builtin_clz(p); i >= 0; --i) {
+            r = r * r;
+            if (p & (1 << i)) r = r * v;
+        }
+        return r;
     }
 }
