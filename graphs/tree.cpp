c9887daa7e4bfa8bf5793d76c94c407eeb259c1e
Saketh Are <saketh.are@gmail.com>
2019-10-29 10:14:04 -0400

    tree: use reference_wrapper for edge lists

--- graphs/tree.cpp
+++ graphs/tree.cpp
@@ -1,55 +1,50 @@
 template<typename E> struct tree {
     int V;
     vvi nbrs;
-    vector<E> edges;
-    vector<vector<E>> adj;
+    vector<vector<reference_wrapper<E>>> edges;
+    vector<E> edge_list;
 
-    vi subt_sz;
-    vb erased;
-
-    tree() {}
-    tree(int _V) : V(_V) {
-        edges.resz(V-1);
-        nbrs.resz(V), adj.resz(V), subt_sz.resz(V), erased.resz(V);
-    }
-    tree(const vector<E>& __edges) : tree(sz(__edges) + 1) {
-        edges = __edges;
-        init();
-    }
-
-    void init() {
-        F0R (i, sz(edges)) {
-            auto& e = edges[i]; e.i = i;
+    tree(int _V = 0) : V(_V) {}
+    tree(const vector<E>& __edge_list) : V(sz(__edge_list) + 1), edge_list(__edge_list) {
+        nbrs.resize(V), edges.resize(V);
+        for (E& e : edge_list) {
             assert(0 <= e.u && e.u < V && 0 <= e.v && e.v < V);
-            adj[e.u].pb(e), nbrs[e.u].pb(e.v);
-            adj[e.v].pb(e), nbrs[e.v].pb(e.u);
+            nbrs[e.u].push_back(e.v);
+            nbrs[e.v].push_back(e.u);
+            edges[e.u].pb(e);
+            edges[e.v].pb(e);
+        }
+        erased.resize(V), subt_sz.resize(V);
+    }
+
+    friend void re(tree& t) {
+        assert(t.V > 0);
+        vector<E> __edge_list(t.V - 1);
+        re(__edge_list);
+        t = tree<E>(__edge_list);
+    }
+    friend void pr(const tree& t) { pr("{V=", t.V, " ", t.edge_list, "}"); }
+
+    vb erased;
+    void erase(int u) { erased[u] = true; }
+
+    mutable vi subt_sz;
+    void calc_subt_sz(int u, bool avoid_erased, int p) const {
+        subt_sz[u] = 1;
+        for (int v : nbrs[u]) if (v != p && (!avoid_erased || !erased[v])) {
+            calc_subt_sz(v, avoid_erased, u);
+            subt_sz[u] += subt_sz[v];
         }
     }
 
-    friend void re(tree& t) { re(t.edges); t.init(); }
-    friend void pr(const tree& t) { pr("{V=", t.V, " ", t.edges, "}"); }
-
-    void erase(int v) { erased[v] = true; }
-
-    void calc_subt_sz(int v, int p = -1) {
-        subt_sz[v] = 1;
-        trav (e, adj[v]) {
-            int u = e[v];
-            if (erased[u] || u == p) continue;
-            calc_subt_sz(u, v);
-            subt_sz[v] += subt_sz[u];
-        }
-    }
-
-    int centroid(int v) {
-        calc_subt_sz(v);
-        int c = v;
-        FIND: trav (e, adj[c]) {
-            int u = e[c];
-            if (!erased[u] && subt_sz[u] < subt_sz[c] && 2 * subt_sz[u] >= subt_sz[v]) {
-                c = u; goto FIND;
+    vi centroids(int r = 0, bool avoid_erased = 1) const {
+        calc_subt_sz(r, avoid_erased, -1);
+        int c = r, p = -1;
+        FIND: for (int u : nbrs[c]) if (!avoid_erased || !erased[u]) {
+            if (subt_sz[u] < subt_sz[c] && 2 * subt_sz[u] >= subt_sz[r]) {
+                p = c, c = u; goto FIND;
             }
         }
-        return c;
+        return subt_sz[c] * 2 == subt_sz[r] ? vi{c,p} : vi{c};
     }
 };
