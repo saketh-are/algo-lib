4de9607f5a087cb67302d410383f50d2e90fb2c9
Saketh Are <saketh.are@gmail.com>
2019-11-08 09:35:49 -0500

    frac: take primitive type as template argument

--- numeric/frac.cpp
+++ numeric/frac.cpp
@@ -1,9 +1,9 @@
-struct frac {
-    ll n, d;
-    frac(ll _n, ll _d = 1) : n(_n), d(_d) {
+template<typename v_t = long long> struct frac {
+    v_t n, d;
+    frac(v_t _n, v_t _d = 1) : n(_n), d(_d) {
         assert(n != 0 || d != 0);
         if (d < 0) { n *= -1; d *= -1; }
-        ll g = __gcd(abs(n), d);
+        v_t g = __gcd(abs(n), d);
         n /= g;
         d /= g;
     }
@@ -44,11 +44,11 @@ struct frac {
     friend frac operator / (const frac& a, const frac& b) { return frac(a) /= b; }
 
     // canonical continued fraction
-    vll to_cont() const {
+    vector<v_t> to_cont() const {
         if (d == 0) return { LLONG_MAX };
-        vll cont;
-        for (ll n = this->n, d = this->d; ; swap(n, d)) {
-            ll f = (n >= 0 ? n : n - d + 1) / d;
+        vector<v_t> cont;
+        for (v_t n = this->n, d = this->d; ; swap(n, d)) {
+            v_t f = (n >= 0 ? n : n - d + 1) / d;
             cont.push_back(f);
             n -= f * d;
             if (n == 0) break;
@@ -56,8 +56,8 @@ struct frac {
         return cont;
     }
 
-    static frac from_cont(const vll& cont) {
-        ll n = 1, d = 0;
+    static frac from_cont(const vector<v_t>& cont) {
+        v_t n = 1, d = 0;
         for (int i = int(cont.size()) - 1; i >= 0; i--) {
             swap(n, d);
             n += d * cont[i];
@@ -69,15 +69,15 @@ struct frac {
     friend frac best_in(const frac x, const frac y) {
         assert(x < y);
 
-        vll x1 = x.to_cont(), y1 = y.to_cont();
-        vll x2 = x1; x2.back()--; x2.push_back(1);
-        vll y2 = y1; y2.back()--; y2.push_back(1);
+        vector<v_t> x1 = x.to_cont(), y1 = y.to_cont();
+        vector<v_t> x2 = x1; x2.back()--; x2.push_back(1);
+        vector<v_t> y2 = y1; y2.back()--; y2.push_back(1);
 
-        auto z = [](const vll& a, const vll& b) {
-            vll c;
+        auto z = [](const vector<v_t>& a, const vector<v_t>& b) {
+            vector<v_t> c;
             for (int i = 0; ; i++) {
-                ll ai = i < a.size() ? a[i] : LLONG_MAX;
-                ll bi = i < b.size() ? b[i] : LLONG_MAX;
+                v_t ai = i < a.size() ? a[i] : LLONG_MAX;
+                v_t bi = i < b.size() ? b[i] : LLONG_MAX;
                 if (ai != bi) {
                     c.push_back(min(ai, bi) + 1);
                     return from_cont(c);
