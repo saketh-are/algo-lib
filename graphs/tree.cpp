f1bbc64f423c0cebd4b3a606574fa7aaa84dc71e
Saketh Are <saketh.are@gmail.com>
2019-12-01 12:37:38 -0500

    tree: add support for more input formats

--- graphs/tree.cpp
+++ graphs/tree.cpp
@@ -1,3 +1,16 @@
+template<typename W=monostate> struct wedge {
+    int u, v, i; W w;
+    wedge<W>(int _u=-1, int _v=-1, int _i=-1, W _w = W{}) : u(_u), v(_v), i(_i), w(_w) {}
+    int operator[](int loc) const { return u ^ v ^ loc; }
+    friend void pr(const wedge& e) { pr(e.u, "<-", e.w, "->", e.v); }
+
+    template<bool FIRST_INDEX = 1>
+    friend void re(wedge& e) { re(e.u, e.v, e.w); if (FIRST_INDEX) --e.u, --e.v; }
+    template<bool FIRST_INDEX = 1>
+    friend void re(wedge& e, int _u) { e.u = _u; re(e.v, e.w); if (FIRST_INDEX) --e.v; }
+};
+
+enum INPUT_FORMAT { EDGE_LIST, PARENT_LIST };
 template<typename E> struct tree {
     int V, root;
 
@@ -7,7 +20,7 @@ template<typename E> struct tree {
     vi preorder, reverse_preorder;
 
     vector<E> edge_list;
-    vector<vector<reference_wrapper<E>>> edges;
+    vector<vector<E>> edges;
 
     vb erased;
     void erase(int u) { erased[u] = true; }
@@ -49,11 +62,16 @@ template<typename E> struct tree {
         for (int v : children[u]) build_preorder(v);
     }
 
+    template<INPUT_FORMAT FMT = EDGE_LIST, bool FIRST_INDEX = 1>
     friend void re(tree& t) {
         assert(t.V > 0);
         vector<E> __edge_list(t.V - 1);
-        re(__edge_list);
-        for (int i = 0; i < t.V - 1; i++) __edge_list[i].i = i;
+        for (int i = 0; i < t.V - 1; i++) {
+            E& e = __edge_list[i];
+            if (FMT == EDGE_LIST) re<FIRST_INDEX>(e);
+            else re<FIRST_INDEX>(e, i + 1);
+            e.i = i;
+        }
         t = tree<E>(__edge_list);
     }
     friend void pr(const tree& t) { pr("{V=", t.V, " ", t.edge_list, "}"); }
